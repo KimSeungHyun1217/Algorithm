ffbe73ec075fe9ff6d36c2ae5fc972a33b926c57
hyun <rlatmdguss97@gmail.com>
2022-02-24 23:53:28 +0900

    #baekjoon#1874

---
+++ baekjoon/1874/스택 수열.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+maxnum = int(input()) #max 값을 받음
+flag= False
+top = 0
+stack_list = []
+answer_list = []
+for i in range(maxnum):
+    num = int(input())
+    if num > top:
+        for i in range(top+1,num+1):  #입력 받은 값이 top보다 높을 경우
+            stack_list.append(i)      #필요한 만큼 값을 더 추가
+            answer_list.append("+")
+        answer_list.append("-")       #마지막 값만 빼감
+        stack_list.pop()
+        top = num
+    else:                             #입력 받은 값이 top보다 낮을 경우
+        while(True):
+            if len(stack_list) == 0:  #stack이 비어있으면 NO만 저장
+                answer_list=[]
+                answer_list.append("NO")
+                flag = True
+                break
+            x = stack_list.pop()       #입력받은 값까지 pop해줌
+            answer_list.append("-")
+            if x == num:
+                break
+    if flag:
+        break
+for i in answer_list:
+    print(i)
