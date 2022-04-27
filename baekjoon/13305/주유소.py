b517fb4f605ae55054298b9a8bf45f89f4de6aac
hyun <juseng12@naver.com>
2022-04-27 16:37:57 +0900

    baekjoon#13305

--- baekjoon/13305/주유소.py
+++ baekjoon/13305/주유소.py
@@ -7,9 +7,11 @@ use = list(map(int,input().split()))
 charge = list(map(int,input().split()))
 
 ans = use[0] * charge[0]
-check = charge[0]
-for i in range(1,len(charge)-1):
+check = charge[0]                
+for i in range(1,len(charge)-1): 
     if check > charge[i]:
         check = charge[i]
     ans += use[i] * check
+    
+print(ans)
     
