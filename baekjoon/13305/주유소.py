f6c718fb0dd709b766a200cc98dcf8fb1f373113
hyun <juseng12@naver.com>
2022-04-27 13:53:20 +0900

    baekjoon#13305

---
+++ baekjoon/13305/주유소.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+use = list(map(int,input().split()))
+charge = list(map(int,input().split()))
+
+ans = use[0] * charge[0]
+check = charge[0]
+for i in range(1,len(charge)-1):
+    if check > charge[i]:
+        check = charge[i]
+    ans += use[i] * check
+    
