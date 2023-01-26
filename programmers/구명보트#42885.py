9185815b6dfae2418dade609a2c54165e8abb56d
hyun <juseng12@naver.com>
2023-01-27 03:03:10 +0900

    programmers#42885

---
+++ programmers/구명보트#42885.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    people.sort(reverse=True)
+    
+    people = deque(people)
+    while len(people) > 1:
+        if people[0] + people[-1] <= limit:
+            answer += 1
+            people.popleft()
+            people.pop()
+        else:
+            answer += 1
+            people.popleft()
+            
+    if people: answer += 1
+    return answer
